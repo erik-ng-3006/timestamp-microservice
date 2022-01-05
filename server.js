4a157811c94812f9d9c61c594105bb22d2d83d16
erik-ng-3006 <erikng3006@gmail.com>
2022-01-05 10:24:46 +0100

    first commit

--- server.js
+++ server.js
@@ -2,31 +2,73 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
+
+
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+app.get("/api", function(req, res) {
+    const d = new Date();
+    res.json({
+        unix: Date.now(),
+        utc: d.toUTCString()
+    })
 });
 
+app.get("/api/:date?", function(req, res) {
+    const regex1 = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+    const regex2 = /\d+/
 
+    function isValidDate(path) {
+        if (!regex1.test(path) || !regex2.test(path)) {
+            return false
+        }
+        return true;
+    };
+
+    const d = req.params.date;
+
+    if (regex1.test(d) === true) {
+        const unixTime = Date.parse(d)
+        const date = new Date(+unixTime)
+        res.json({
+            unix: unixTime,
+            utc: date.toUTCString()
+        })
+    }
+    if (regex2.test(d) === true) {
+        const date = new Date(+d)
+        res.json({
+            unix: parseInt(d),
+            utc: date.toUTCString()
+        })
+    }
+    if (!isValidDate(d)) {
+        res.json({ "error": "Invalid Date" })
+    }
+
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT || 8080, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
 });
